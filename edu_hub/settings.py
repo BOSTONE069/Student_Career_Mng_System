0d4a4350ae7de3338442fc399f7475613cfa5500
Timothy Wamalwa <timwamalwa@gmail.com>
2025-06-04 12:28:12 +0300

    Updated db connection

--- edu_hub/settings.py
+++ edu_hub/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'edu_hub.wsgi.application'
 # }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
