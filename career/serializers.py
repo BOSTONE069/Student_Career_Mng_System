6e413f87598155a26a83bd8f55552d2b858b9264
BOSTONE069 <bostoneochieng69@gmail.com>
2025-06-05 21:32:13 +0300

    Added comments into the career app of project

--- career/serializers.py
+++ career/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from .models import CareerAssessment, CareerRecommendation
 
+# The `CareerAssessmentSerializer` class is a Django REST framework serializer for the
+# `CareerAssessment` model with all fields included.
 class CareerAssessmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = CareerAssessment
         fields = '__all__'
 
+# The `CareerRecommendationSerializer` class is a Django REST framework serializer for the
+# `CareerRecommendation` model with all fields included.
 class CareerRecommendationSerializer(serializers.ModelSerializer):
     class Meta:
         model = CareerRecommendation
